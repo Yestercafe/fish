89255f43a35295389dada274d74b308813253cb2
qyc027 <qyc027@gmail.com>
2023-02-04 22:15:00 +0800

    Initial commit

---
+++ config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias q=vim
+alias g=git
+alias ga="git add"
+alias gaa="git add -A"
+alias gcmsg="git commit -m"
+alias gp="git push"
+alias gpf="git push -f"
+alias gl="git pull"
+alias gst="git status"
+alias glog="git log --graph --oneline"
+
