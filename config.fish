eacc643fc9a9a8a90cd4fd716e0ea2a448edbba4
qyc027 <qyc027@gmail.com>
2023-02-04 22:23:41 +0800

    Update

--- config.fish
+++ config.fish
@@ -2,7 +2,14 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+## basic aliases
 alias q=vim
+function mcd
+    mkdir $argv
+    cd $argv
+end
+
+## git
 alias g=git
 alias ga="git add"
 alias gaa="git add -A"
@@ -12,4 +19,7 @@ alias gpf="git push -f"
 alias gl="git pull"
 alias gst="git status"
 alias glog="git log --graph --oneline"
+
+## PATH
+set -x PATH ~/dwm-things/bin $PATH
 
